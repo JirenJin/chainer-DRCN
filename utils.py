b64433c5b22f23d44f06ed5d255406d7fa8d51c4
Jiren Jin <jinjiren@foxmail.com>
2018-04-10 13:25:02 +0900

    add datetime

--- utils.py
+++ utils.py
@@ -1,16 +1,19 @@
 """
 Contains all helpers for DRCN
 """
+import datetime
 import json
 import os
 
 
 def prepare_dir(args):
     # customize the output path
+    date = datetime.datetime.now()
+    date_str = date.strftime("%m%d%H%M%S")
     if args.source_only:
-        out_path = os.path.join(args.out, 'source_only')
+        out_path = os.path.join(args.out, 'source_only', date_str)
     else:
-        out_path = os.path.join(args.out, args.noise)
+        out_path = os.path.join(args.out, args.noise, date_str)
     try:
         os.makedirs(out_path)
     except OSError:
